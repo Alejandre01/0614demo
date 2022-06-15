8095181d055c7fb5f6abd3c7962e1ce81c621633
Bernardo Alejandre <bernardo.alejandre@gmail.com>
2022-06-15 11:02:12 -0700

    BACK

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Let's do this</Text>
+      <Text>I am back</Text>
       <StatusBar style="auto" />
     </View>
   );
