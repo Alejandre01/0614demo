b2a5e123dd4e150e84b40e3e2843b5e0d67acd24
Bernardo Alejandre <bernardo.alejandre@gmail.com>
2022-06-15 11:26:07 -0700

    Double Check

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>I am back</Text>
+      <Text>I am back doubleck</Text>
       <StatusBar style="auto" />
     </View>
   );
