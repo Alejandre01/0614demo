04f5d400647f14c90a5edbf2455781e3b9e72619
Bernardo Alejandre <bernardo.alejandre@gmail.com>
2022-06-15 01:31:30 -0700

    do it

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>my name is Bernardo</Text>
+      <Text>Let's do this</Text>
       <StatusBar style="auto" />
     </View>
   );
